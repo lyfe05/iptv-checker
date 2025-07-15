6f583c467603db44c6d03f8633b4d68e25160066
lyfe05 <mastercarder.169@gmail.com>
2025-07-15 19:27:52 +0300

    Create Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header style={{ padding: "20px", backgroundColor: "#111", color: "#fff" }}>
+      <h1>⚽ Live Football Streaming</h1>
+    </header>
+  );
+};
+
+export default Header;
